df788e69c016eb697d834ccd448ae41bac225ae0
Christian Ezeani <christian.ezeani@gmail.com>
2019-08-08 00:44:20 +0100

    Implement SitemapIndexFeeder

--- src/Entries/SitemapIndexFeederEntry.php
+++ src/Entries/SitemapIndexFeederEntry.php
@@ -2,7 +2,50 @@
 namespace SimpleFeeder\Entries;
 
 use SimpleFeeder\Core\Entry;
+use SimpleFeeder\Traits\UsesDomFunctions;
+use DOMDocument, DOMElement;
 
 class SitemapIndexFeederEntry extends Entry {
 
+  use UsesDomFunctions;
+
+  /**
+   * @var DOMElement
+   */
+  private $parent;
+
+  /**
+   * @var DOMElement
+   */
+  private $loc, $lastmod;
+
+  function __construct(DOMDocument $dom, DOMElement $parent) {
+    $this->dom = &$dom;
+    $this->parent = &$parent;
+
+    $this->root = $dom->createElement('sitemap');
+    $parent->appendChild($this->root);
+  }
+
+  function __destruct() {
+    $this->parent->removeChild($this->root);
+  }
+
+  public function getLocValue() {
+    return $this->getValue($this->loc);
+  }
+
+  public function setLocValue($value, $attributes) {
+    $this->setValueToRoot($this->loc, 'loc', $value, $attributes);
+  }
+
+  public function getLastmodValue() {
+    return $this->getValue($this->lastmod);
+  }
+
+  public function setLastmodValue($value, $attributes) {
+    $this->setValueToRoot($this->lastmod, 'lastmod', $value, $attributes);
+  }
+
 }
+
--- src/Feeders/SitemapIndexFeeder.php
+++ src/Feeders/SitemapIndexFeeder.php
@@ -10,6 +10,22 @@ class SitemapIndexFeeder extends Feeder {
   use UsesDomReaderWriter;
 
   protected $entryType = SitemapIndexFeederEntry::class;
+
+  protected function onDomReady() {
+    $this->root = $this->dom->createElement('sitemapindex');
+    $this->root->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+    $this->dom->appendChild($this->root);
+  }
+
+  protected function onBeforeRender() {
+    if (!headers_sent()) {
+      header('Content-Type: text/xml');
+    }
+  }
+
+  protected function newEntryParameters() {
+    return array(&$this->dom, &$this->root);
+  }
   
 }
 
